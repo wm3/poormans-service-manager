ede28cfc1b84eb2d6a9f7d42f769133eb463dbf9
Motoi Washida <wm3@mail.w3ch.jp>
2012-05-19 21:52:31 +0900

    Better request handling

--- src/main/scala/service/Daemon.scala
+++ src/main/scala/service/Daemon.scala
@@ -2,6 +2,11 @@ package jp.w3ch.psm.service
 
 import java.net.InetSocketAddress
 
+import com.twitter.conversions.time._
+import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.service.{Backoff,RetryPolicy}
+import com.twitter.finagle.http.{Http,Request,Method}
+import com.twitter.util.{Future,Promise}
 import org.jboss.netty.handler.codec.http._
 
 import jp.w3ch.psm.daemon
@@ -12,13 +17,30 @@ class Daemon(command:String, address:InetSocketAddress) extends HttpService {
   val executor = daemon.Daemon(command)
   val port = new Port(address)
 
+  lazy val exec = executor.exec()
+
   def this(command:String, address: Int) = this(command, new InetSocketAddress(address))
 
   override def apply(request:HttpRequest) = {
+    for {
+      _ <- waitForPort(1)
+             .rescue {
+               case _ => Future(exec).join(waitForPort(4))
+             }
+      response <- port(request)
+    } yield(response)
+  }
 
-    if ( ! executor.isRunning) executor.exec()
+  def waitForPort(retry:Int) = {
+    val client = ClientBuilder()
+      .codec(Http())
+      .hosts(address)
+      .hostConnectionLimit(1)
+      .retryPolicy(
+        RetryPolicy.backoff(Backoff.const(500.milliseconds).take(3))(RetryPolicy.WriteExceptionsOnly))
+      .build()
 
-    port(request)
+    client(Request(Method.Head, "/")).unit
   }
 }
 
