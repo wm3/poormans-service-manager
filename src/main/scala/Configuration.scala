30e2a4ddb39efe91c5ae6d139c970703c2fbc53a
Motoi Washida <wm3@mail.w3ch.jp>
2012-06-02 19:17:30 +0900

    ドキュメントを記載

--- src/main/scala/Configuration.scala
+++ src/main/scala/Configuration.scala
@@ -9,7 +9,11 @@ import com.twitter.conversions.time._
 import org.jboss.netty.handler.codec.http._
 
 class Configuration extends com.twitter.util.Config[Server] with ConfigurationUtil {
-  // parameters
+
+  // ----------------------------------------------------------------
+  //     Parameters
+  // ----------------------------------------------------------------
+
   var listen = required[Int]
   var proxyHandler = required[MyServer.ProxyHandler]
 
@@ -17,7 +21,9 @@ class Configuration extends com.twitter.util.Config[Server] with ConfigurationUt
   def proxy_=(proxy: MyServer.ProxyHandler) { proxyHandler = proxy }
 
 
-  // building process
+  // ----------------------------------------------------------------
+  //     Building process
+  // ----------------------------------------------------------------
 
   override def apply = {
     val sb = ServerBuilder()
@@ -33,6 +39,11 @@ class Configuration extends com.twitter.util.Config[Server] with ConfigurationUt
   }
 }
 
+
+// ----------------------------------------------------------------
+//     Utilities
+// ----------------------------------------------------------------
+
 trait ConfigurationUtil {
   val nullProxyHandler: MyServer.ProxyHandler = { case _ if false => throw new Exception() }
   val port = new service.Port(_:Int)
@@ -41,16 +52,22 @@ trait ConfigurationUtil {
 }
 
 
-// 
-
+/**
+ * (仮想)ホスト名情報を指すクラスです。
+ */
 sealed class HostUrl {}
 case class Host(host:String, port:Int) extends HostUrl {}
 object NoHost extends HostUrl
 
 object HostUrl {
-  val re = """([^/:]+)(?::(\d+))?""".r
-  def get(s: String): Option[String] = { if (s == "") None else Some(s) }
 
+  private[this] val re = """([^/:]+)(?::(\d+))?""".r
+
+  private[this] def get(s: String): Option[String] = { if (s == "") None else Some(s) }
+
+  /**
+   * 文字列からホスト情報を作成します。
+   */
   def apply(name: Option[String]): HostUrl = {
     name match {
       case Some(re(host, port)) => Host(host, get(port).map(_.toInt).getOrElse(80))
