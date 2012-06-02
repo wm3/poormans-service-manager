736a895617843de94f4e5f3306c91a8ab73386ad
Motoi Washida <wm3@mail.w3ch.jp>
2012-06-02 10:35:22 +0900

    実行ファイルの名前を変更

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-EXECUTABLE=target/finagle-test-assembly-0.0.1-SNAPSHOT.jar
+EXECUTABLE=target/poormans-service-manager-assembly-0.0.1-SNAPSHOT.jar
 DEFAULT_JAVA_OPTS='-XX:+UseSerialGC -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=50 -XX:PermSize=8m -Xms3m -server'
 : ${JAVA_OPTS:=$DEFAULT_JAVA_OPTS}
 
