47950444031690d492a83a0578e5a36d51421d8c
Motoi Washida <wm3@mail.w3ch.jp>
2012-06-03 16:37:08 +0900

    リファクタリング

--- src/main/scala/DispatchingServer.scala
+++ src/main/scala/DispatchingServer.scala
@@ -1,29 +1,31 @@
 package jp.w3ch.psm
 
 
-import java.net.InetSocketAddress
-
 import org.jboss.netty.handler.codec.http._
-import org.jboss.netty.handler.codec.http.HttpResponseStatus._
-
 import com.twitter.finagle.Service
 
+
+class DispatchingServer(dispatch: DispatchingServer.ProxyHandler) extends Service[HttpRequest, HttpResponse] {
+
+  val defaultService = new service.TextResponse("hello")
+
+  override def apply(request: HttpRequest) = {
+    val host = Option(request.getHeader("Host"))
+
+    val service = doDispatch(HostUrl(host))
+
+    service(request)
+  }
+
+
+  private[this] val doDispatch: DispatchingServer.ProxyHandler = dispatch orElse {
+    case _ => defaultService
+  }
+
+}
 
 object DispatchingServer {
   type ProxyHandler = PartialFunction[HostUrl, Service[HttpRequest, HttpResponse]]
 }
 
-class DispatchingServer(proxy: DispatchingServer.ProxyHandler) extends Service[HttpRequest, HttpResponse] {
-
-  val defaultServer = new service.TextResponse("hello")
-  val getDefaultServer: DispatchingServer.ProxyHandler = { case _ => defaultServer }
-
-  override def apply(request: HttpRequest) = {
-    val host = Option(request.getHeader("Host"))
-
-    val server = (proxy orElse getDefaultServer)(HostUrl(host))
-
-    server(request)
-  }
-}
 // vim: set shiftwidth=2 expandtab :
